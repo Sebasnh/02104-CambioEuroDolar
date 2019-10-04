e613051aa45f58b592559685613dbec6199acac4
FAMILIA <FAMILIA@192.168.1.56>
2019-10-04 13:55:07 +0200

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Sebastian Najarro Heredia - sebastian.najarro.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Sebastian Najarro Heredia - sebastian.najarro.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+         
+       System.out.print("El valor de 1 euro es equivalente a ");
+       System.out.printf(Locale.ENGLISH,"%.2f",1.1);
+       System.out.print("");
+       System.out.println(" dólares.");
+       
+          
+    }
+   
+    
+}
